1d02283edb738370e1548de95cd758eacdf3c5b8
jdcoupon <50163393+jdcoupon@users.noreply.github.com>
2020-12-19 15:43:38 +0800

    Add files via upload

---
+++ js/bd.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?419c8d33001d26830d72e679a89c3bbc";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
