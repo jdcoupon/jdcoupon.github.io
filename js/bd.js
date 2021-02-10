9018b41acaba1f907ed483c544490a0530437ec7
jdcoupon <50163393+jdcoupon@users.noreply.github.com>
2021-02-10 23:29:39 +0800

    Update and rename bd.js to ym.js

--- js/bd.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?419c8d33001d26830d72e679a89c3bbc";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
